968e831cecaaf99ab340500067498a3f02a74fc5
shivani mehta <shivanimehta21@navgurukul.org>
2023-12-30 17:21:21 +0530

    modal in table format

--- app/MovieModal.js
+++ app/MovieModal.js
@@ -1,18 +1,72 @@
+// import React from "react";
+
+// const MovieModal = ({ movie, onClose }) => {
+//   return (
+//     <div>
+//       <div className="overlay" onClick={onClose}></div>
+//       <div className="modal-container">
+//         <span className="close-btn" onClick={onClose}>
+//           &times;
+//         </span>
+//         {/* Display specific movie details */}
+//         <h2 className="movie-title">{movie.title || movie.name}</h2>
+//         <p>Rating: {movie.vote_average}</p>
+//         <p>Description: {movie.overview}</p>
+//         <p>Language: {movie.original_language}</p>
+//         {/* Additional content... */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MovieModal;
+
+
 import React from "react";
 
 const MovieModal = ({ movie, onClose }) => {
   return (
     <div>
       <div className="overlay" onClick={onClose}></div>
-      <div className="modal-container">
+      <div  className="modal-container" style={{height:'auto'}}>
         <span className="close-btn" onClick={onClose}>
           &times;
         </span>
+
         {/* Display specific movie details */}
         <h2 className="movie-title">{movie.title || movie.name}</h2>
-        <p>Rating: {movie.vote_average}</p>
-        <p>Description: {movie.overview}</p>
-        <p>Language: {movie.original_language}</p>
+
+        {/* Table for movie details */}
+        <table>
+          <tbody>
+            <tr>
+              <td>Name :   </td>
+              <td>{movie.title || movie.name}</td>
+            </tr>
+            <tr>
+              <td>Rating :  </td>
+              <td>{movie.vote_average}</td>
+            </tr>
+            <tr>
+              <td>Description:  </td>
+              <td>{movie.overview}</td>
+            </tr>
+            <tr>
+              <td>Language:  </td>
+              <td>{movie.original_language}</td>
+            </tr>
+            <tr>
+              <td>Popularity :  </td>
+              <td>{movie.popularity}</td>
+            </tr>
+            <tr>
+              <td>Release_Date:</td>
+              <td>  {movie.release_date}</td>
+            </tr>
+            {/* Add more rows for additional details */}
+          </tbody>
+        </table>
+
         {/* Additional content... */}
       </div>
     </div>
